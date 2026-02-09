8c5387260936c8d39343996ebb52507c12a255e4
stephen842 <ugotachisomstephen@gmail.com>
2026-02-09 06:44:01 +0100

    feat: Tested all endpoints and also added full test data for GraphQL endpoints in the project directory(testing.txt) and register all models in admin for easy management.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,67 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, UserProfile, UserSession, IPActivity, BlacklistedIP
 
-# Register your models here.
+# -------------------------------
+# User Admin
+# -------------------------------
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        'email', 'username', 'name', 'is_active',
+        'is_verified', 'is_staff', 'is_platform_admin', 'date_joined'
+    )
+    list_filter = ('is_active', 'is_verified', 'is_staff', 'is_platform_admin')
+    search_fields = ('email', 'username', 'name')
+    ordering = ('-date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'name', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_verified', 'is_staff', 'is_platform_admin', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('date_joined', 'last_login', 'last_activity', 'account_updated_at', 'deactivated_at')}),
+        ('Email updates', {'fields': ('_new_email', 'email_verification_pending')}),
+        ('Deactivation', {'fields': ('is_deactivated',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'name', 'password1', 'password2', 'is_active', 'is_verified', 'is_platform_admin'),
+        }),
+    )
+
+# -------------------------------
+# UserProfile Admin
+# -------------------------------
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'country', 'gender', 'phone_number', 'created_at')
+    search_fields = ('user__email', 'user__username', 'phone_number')
+    list_filter = ('country', 'gender', 'created_at')
+
+# -------------------------------
+# UserSession Admin
+# -------------------------------
+@admin.register(UserSession)
+class UserSessionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'ip_address', 'device_type', 'os', 'browser', 'last_active', 'is_active')
+    search_fields = ('user__email', 'ip_address', 'user_agent')
+    list_filter = ('device_type', 'os', 'browser', 'is_active')
+
+# -------------------------------
+# IPActivity Admin
+# -------------------------------
+@admin.register(IPActivity)
+class IPActivityAdmin(admin.ModelAdmin):
+    list_display = ('ip_address', 'user', 'endpoint', 'method', 'request_count', 'failed_attempts', 'is_suspicious', 'last_seen')
+    search_fields = ('ip_address', 'user__email', 'endpoint')
+    list_filter = ('method', 'is_suspicious')
+
+# -------------------------------
+# BlacklistedIP Admin
+# -------------------------------
+@admin.register(BlacklistedIP)
+class BlacklistedIPAdmin(admin.ModelAdmin):
+    list_display = ('ip_address', 'reason', 'is_active', 'created_at')
+    search_fields = ('ip_address', 'reason')
+    list_filter = ('is_active',)
--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from .models import Post, Comment, Like, Bookmark, Follow, Share, UserAnalytics, PostDailyMetrics
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "author", "created_at", "updated_at")
+    list_filter = ("created_at", "author")
+    search_fields = ("content", "author__username")
+    ordering = ("-created_at",)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "post", "author", "created_at")
+    list_filter = ("created_at", "author")
+    search_fields = ("content",)
+    ordering = ("-created_at",)
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ("user", "post", "created_at")
+    list_filter = ("created_at", "user")
+    search_fields = ("user__username",)
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ("user", "post", "created_at")
+    list_filter = ("created_at", "user")
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ("follower", "following", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("follower__username", "following__username")
+
+@admin.register(Share)
+class ShareAdmin(admin.ModelAdmin):
+    list_display = ("original_post", "shared_by", "created_at")
+    list_filter = ("created_at",)
+
+@admin.register(UserAnalytics)
+class UserAnalyticsAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "total_posts",
+        "total_likes_recieved",
+        "total_comments_recieved",
+        "total_shares_recieved",
+        "updated_at",
+    )
+    search_fields = ("user__username",)
+
+@admin.register(PostDailyMetrics)
+class PostDailyMetricsAdmin(admin.ModelAdmin):
+    list_display = ("post", "date", "likes", "comments", "shares")
+    list_filter = ("date",)
